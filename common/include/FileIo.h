d0b6454a80de8c7d65e5b749b03c5e3e882be2de
MaciejSz2112 <maciejszymanski99@wp.pl>
2026-07-14 19:50:06 +0200

    Separate code for file handling
    - Move file handling code to separate file
    - Switch to use functions instead of macros
    - Fix in code calls for file handling

---
+++ common/include/FileIo.h
@@ -0,0 +1,46 @@
+#ifndef FILE_IO_H
+#define FILE_IO_H
+
+#include <CommonTypes.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+/**
+  @brief Opens a file stream
+
+  @param[out]   FilePtr     Pointer to the FILE pointer to be populated.
+  @param[in]    FileName    Path to the file.
+  @param[in]    Mode        Access mode.
+
+  @return   STATUS_SUCCESS, or appropriate error code.
+**/
+Status OpenFileStream (
+  FILE          **FilePtr,
+  const char    *FileName,
+  const char    *Mode
+  );
+
+
+/**
+  @brief Closes an open file stream
+
+  @param[in]    FilePtr     Pointer to the open FILE stream.
+  @param[in]    FileName    Path to the file.
+
+  @return   STATUS_SUCCESS, or appropriate error code.
+**/
+Status CloseFileStream (
+  FILE          *FilePtr,
+  const char    *FileName
+  );
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif    //  FILE_IO_H
